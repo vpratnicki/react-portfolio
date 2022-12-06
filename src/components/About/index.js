479055a36c73c01adaeb159f191171143255ab8c
vpratnicki <vanessa@pratnicki.net>
2022-12-05 21:20:19 -0500

    Chore: Initial About and Nav components setup.

---
+++ src/components/About/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function About() {
+  return (
+    <section id="about">
+      <div class="about-section">
+        <div class="about-text">
+          <h2>About</h2>
+          <p> I enjoy making the internet better one page at a time!</p>
+        <br/>
+        <h3>Background</h3>
+        <p>I have over 8 years of experience as a Web Content Manager, where I create and execute effective website
+          design, digital writing and editing. I have experience crafting clear, concise messaging; ensuring content is
+          current, relevant and conforms to industry best practices, as well as adheres to the Americans with
+          Disabilities Act (ADA) Standards for Accessible Design. My communication skills and demonstrated ability to
+          develop effective web content has been a great asset.</p>
+          <br/>
+        <p>
+          While I enjoy my career as a Web Content Manager, it is a desire of mine to cross over to the back-end side
+          and become a Web Developer! To do this, I have enrolled in the George Washington Coding Boot Camp.
+        </p>
+
+
+        
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default About;
