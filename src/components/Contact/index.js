c33ea3fe992905772e5e6e33248c8cb3312fed72
vpratnicki <vanessa@pratnicki.net>
2022-12-10 12:57:44 -0500

    Chore: markup structure of the contact form.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,21 @@ function Contact() {
     return (
         <div>
             <h2>Contact</h2>
+            <form id="contact-form">
+                <div>
+                    <label htmlFor="name">Name:</label>
+                    <input type="text" name="name" />
+                </div>
+                <div>
+                    <label htmlFor="email">Email address:</label>
+                    <input type="email" name="email" />
+                </div>
+                <div>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" rows="5" />
+                </div>
+                <button type="submit">Submit</button>
+            </form>
         </div>
     );
 }
