8b26d42b3951aee26dd946a6aeaab81e6c32c0c6
vpratnicki <vanessa@pratnicki.net>
2022-12-10 14:16:22 -0500

    Chore: initialized & Synced the form state.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Contact() {
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+
+    function handleChange(e) {
+        setFormState({...formState, [e.target.name]: e.target.value })
+      }
+
     return (
         <div>
             <h2>Contact</h2>
             <form id="contact-form">
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
             </form>
