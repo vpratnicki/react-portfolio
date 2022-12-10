800d79a7090807d383ef45f3e0952d5af69494c3
vpratnicki <vanessa@pratnicki.net>
2022-12-10 14:22:27 -0500

    Chore: submit the form data.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,12 +6,17 @@ function Contact() {
 
     function handleChange(e) {
         setFormState({...formState, [e.target.name]: e.target.value })
-      }
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
 
     return (
         <div>
             <h2>Contact</h2>
-            <form id="contact-form">
+            <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
                     <input type="text" defaultValue={name} onChange={handleChange} name="name" />
