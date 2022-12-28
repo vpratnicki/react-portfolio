8b7d97dfe10502f83ce2e5416462b73a3979bd0c
vpratnicki <vanessa@pratnicki.net>
2022-12-28 13:55:15 -0500

    Chore: updated About styles.

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -7,7 +7,7 @@ function About() {
     <section className="hero">
       <div className="hero-intro">
         <h1>Cats, Coffee and Code!</h1>
-        <p> These are a few of my favorite things!</p>
+        <p> All things are possible with a cat on your lap,<br/> coffee in your mug and code on your laptop. </p>
       </div>
     </section>
 
@@ -15,9 +15,14 @@ function About() {
       <div className="about-section">
         <div className="about-text">
           <h2>About</h2>
-          <p> I enjoy making the internet better one page at a time!</p>
+            <p>  
+              
+            I am dedicated to staying up to date with the latest trends and technologies in website design and committed to bringing state-of-the-art web approaches.
+             
+            </p>
         <br/>
         <h3>Background</h3>
+        
         <p>I have over 8 years of experience as a Web Content Manager, where I create and execute effective website
           design, digital writing and editing. I have experience crafting clear, concise messaging; ensuring content is
           current, relevant and conforms to industry best practices, as well as adheres to the Americans with
@@ -32,9 +37,11 @@ function About() {
         <div className="fun-facts">
         <strong><h4>Fun Facts</h4></strong>
         <ul>
-          <li>You will most likely never see me without a coffee, and I always have a dad joke ready to share.</li>
-          <li>Cats are my spirit animals, I was a crazy cat lady as early as middle school. Currently I have 3, trying to convince my hubby we need one more!
+          <li>You will most likely never see me without a coffee and I always have a dad joke ready to share.</li>
+          <li>Cats are my spirit animals, I was a crazy cat lady as early as middle school. Currently I have 3, but I think we need one more!
           </li>
+          <li>The web has always been a part of my life. As a child, I recall going on photo shoots with my mom for the sites she was building. In high school, I studied interactive media and was a part of the website club that created the first website for our high school.
+</li>
         </ul>
       </div>
         <br/>
