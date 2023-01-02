269ff49f0fef216d3096f9b2161bcfbe808b86d8
vpratnicki <vanessa@pratnicki.net>
2023-01-01 23:13:03 -0500

    Bug fix: About content loads as homepage.

--- src/components/PortfolioContainer/index.js
+++ src/components/PortfolioContainer/index.js
@@ -34,12 +34,14 @@ function PortfolioContainer() {
       <Router>
         <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
         <Routes>
+          <Route path="react-portfolio/" element={<About />} />
           <Route path="/" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
+      
       {/* {renderPage()} */}
       <Footer />
     </div>
